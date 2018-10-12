33fe0e2a477dc598d0ad89c1ef8b1c1fe831162e
blairstanek <ablairs1@jhu.edu>
2018-10-11 21:46:40 -0400

    Update basic_earley.py
    Re-wrote the basic Earley parsing algorithm to use the 3-function predictor/scanner/attach approach.  Also moved to 4-space indentation.  Tested on papa.gr against lect10. Still does NOT handle probabilities, does NOT choose the best parse, does NOT carve out terminals separately to reduce N.

--- basic_earley.py
+++ basic_earley.py
@@ -3,111 +3,144 @@
 import sys
 import numpy
 
-# Represents a grammar rule read in from .GR file
+# This class represents a single grammar rule read in from .GR file
 class GrRule:
-  def __init__(self, prob, lhs, rhs):
-    self.prob = prob
-    self.lhs = lhs
-    self.rhs = rhs
+    def __init__(self, prob, lhs, rhs):
+        self.prob = prob
+        self.lhs = lhs
+        self.rhs = rhs
 
-  def print(self):
-    print("   " + self.lhs + " --> ", end="")
-    for s in self.rhs:
-      print(s, end="\t")
+    def print(self, index_period = -1):
+        print("   " + self.lhs + " --> ", end="")
+        for i in range(0, len(self.rhs)):
+            if i == index_period:
+                print(" . ", end="")
+            print(self.rhs[i], end="  ")
 
-def read_grammar(filename):
-  """Read grammar rules from an external file.
-  The rules are read into a list of GrRule.
-  """
-  grammar_rules = []
-  with open(filename) as infile:
-    for line in infile:
-      if len(line) > 2:
-        arr = line.split()
-        prob = float(arr.pop(0))
-        lhs = arr.pop(0)
-        grammar_rules.append(GrRule(prob, lhs, arr))
-
-  return grammar_rules
+        if index_period == len(self.rhs):
+            print(" . ", end="")
 
 
-# Represents a single entry (i.e., the rule, the start index, and the period index)
+# This class represents a single entry (i.e., the rule, the start index, and the period index)
 class Entry:
-  def __init__(self, rule_index, start_index, period_index):
-    self.rule_index = rule_index
-    self.start_index = start_index
-    self.period_index = period_index
+    def __init__(self, rule_index, start_index, period_index):
+        self.rule_index = rule_index
+        self.start_index = start_index
+        self.period_index = period_index
 
-  def print(self):
-    print("  start_index " + str(self.start_index) + " period_index " + str(self.period_index))
+
+
+# This class represents the entire parser
+class EarleyParser:
+    def __init__(self):
+        self.grammar_rules = None
+        self.num_rules = -1
+        self.chart = None
+        self.states_added = None
+
+    # Read grammar rules from an external file.
+    # The rules are read into a list of GrRule.
+    def read_grammar_rules(self, grammar_filename):
+        self.grammar_rules = []
+        with open(grammar_filename) as infile:
+            for line in infile:
+                if len(line) > 2:
+                    arr = line.split()
+                    prob = float(arr.pop(0))
+                    lhs = arr.pop(0)
+                    self.grammar_rules.append(GrRule(prob, lhs, arr))
+        self.num_rules = len(self.grammar_rules)
+
+    # This is the first operator in Earley (out of three), see J&M p.444
+    # It expands a possible operator into multiple
+    def predictor(self, state, i_col, next_cat):
+        for i_rule in range(0, len(self.grammar_rules)):
+            if self.grammar_rules[i_rule].lhs == next_cat:
+                new_entry = Entry(i_rule, i_col, 0)
+                self.enqueue(new_entry, i_col, "PREDICTOR") # attempt to add new state, if not already added
+
+
+    # This is the second operator in Earley (out of three), see J&M p.444
+    # It puts a new completed entry in the NEXT column of the chart
+    def scanner(self, state, i_col):
+        new_entry = Entry(state.rule_index, state.start_index, 1)
+        self.enqueue(new_entry, i_col +1, "SCANNER")
+
+
+    # This is the third operator in Earley, called "Completer" by J&M p.444
+    # It goes back to PRIOR chart entries to find "customers" for a completed state
+    def attach(self, state, i_col):
+        match_seeking = self.grammar_rules[state.rule_index].lhs
+        icol2 = state.start_index
+        for irow2 in range(0, len(self.chart[icol2])):
+            entry2 = self.chart[icol2][irow2]
+            if entry2.period_index < len(self.grammar_rules[entry2.rule_index].rhs):
+                # then this may be seeking a completion
+                possible_match = self.grammar_rules[entry2.rule_index].rhs[entry2.period_index]
+                if possible_match == match_seeking:  # if this is true, we have a "customer" to "attach"
+                    new_entry = Entry(entry2.rule_index, entry2.start_index,
+                                      entry2.period_index + 1)
+                    self.enqueue(new_entry, i_col, "COMPLETER")
+
+
+    # This is a crucial helper function in Earley, see J&M p.444
+    # It tries to add a state to the chart a column i_col.
+    # It only adds that state if it has not already been added in i_col.
+    def enqueue(self, state, column, which_function):
+        tuple_version_of_state = (state.rule_index, state.start_index, state.period_index)
+        if tuple_version_of_state not in self.states_added[column]:
+            self.chart[column].append(state)
+            self.states_added[column][tuple_version_of_state] = True
+
+            print(which_function + " resulted in adding the following at Col = " +
+                    str(column) + " Row = " + str(len(self.chart[column])))
+            print(str(state.start_index), end="  ")
+            self.grammar_rules[state.rule_index].print(state.period_index)
+            print(" (period_index " + str(state.period_index) + ")")
+
+
+    def parse(self, sentence_filename):
+        sen_file = open(sentence_filename)  # open .SEN file
+        for sentence in sen_file:
+            if len(sentence.strip()) > 2:
+                words = sentence.split()
+
+                self.chart = [[] for x in range(0, len(words)+1)] # create the chart
+                self.states_added = [{} for x in range(0, len(words)+1)] # list of dictionaries for state used
+                self.enqueue(Entry(0, 0, 0), 0, "DUMMY START STATE") # add the first rule to the start of the chart
+
+                for i_col in range(0, len(words)+1):  # iterates over columns in Earley chart
+
+                    i_row = 0  # this index into chart[i] keeps track of which item remains to predict or scan
+                    while i_row < len(self.chart[i_col]):  # chart[i] can have additional items added during this loop
+                        state = self.chart[i_col][i_row]
+                        len_rhs = len(self.grammar_rules[state.rule_index].rhs)
+                        period_index = state.period_index
+
+                        if period_index > len_rhs:  # this means there is an error
+                            sys.exit("ERROR: period_index > len_rhs")
+
+                        incomplete = period_index < len_rhs # an entry is "complete" if all rules are left of the period
+
+                        if incomplete:
+                            next_cat = self.grammar_rules[state.rule_index].rhs[period_index]
+
+                            if i_col < len(words) and next_cat == words[i_col]:
+                                self.scanner(state, i_col)
+                            else:
+                                self.predictor(state, i_col, next_cat)
+                        else:  # if we are here, we have a completed item and we need to run ATTACH (a/k/a COMPLETE)
+                            self.attach(state, i_col)
+
+                        i_row += 1
+
 
 # This main function coordinates all the code to run
 def main():
-  if len(sys.argv) != 3:
-    sys.exit("Usage: basic_earley grammar.gr sentences.sen")
-  grammar_rules = read_grammar(sys.argv[1])
-
-  with open(sys.argv[2]) as sen_file:
-    for sentence in sen_file:
-      if len(sentence.strip()) > 2:
-        words = sentence.split()
-        N = len(words) + 1
-
-        arr = [[] for x in range(0,N)]
-        arr[0].append(Entry(0,0,0))
-
-        rules_added = numpy.empty((len(grammar_rules), N), dtype=bool)
-        rules_added.fill(False)
-        rules_added[0][0] = True
-
-        for icol in range(0, N): # iterates over columns in Earley table
-
-          irow = 0  # this index into arr[i] keeps track of which item remains to predict or scan
-          while irow < len(arr[icol]): # arr[i] can have additional items added in this loop
-            len_rhs = len(grammar_rules[arr[icol][irow].rule_index].rhs)
-            period_index = arr[icol][irow].period_index
-            if period_index > len_rhs: # this means there is an error
-              sys.exit("ERROR: period_index > len_rhs")
-            elif period_index < len_rhs: # This means NOT finished
-              entry_to_expand = grammar_rules[arr[icol][irow].rule_index].rhs[period_index]
-
-              if icol < len(words) and entry_to_expand == words[icol]: # match to nonterminal
-                new_entry = Entry(arr[icol][irow].rule_index, arr[icol][irow].start_index, 1)
-                arr[icol+1].append(new_entry)
-                rules_added[new_entry.rule_index][icol+1] = True
-
-                print("SCANNER Added in Col=" + str(icol+1) + " Row=" + str(len(arr[icol+1])))
-                grammar_rules[new_entry.rule_index].print()
-                new_entry.print()
-
-              else:
-                for irule in range(0, len(grammar_rules)):
-                  if not rules_added[irule][icol]:
-                    if grammar_rules[irule].lhs == entry_to_expand:
-                      new_entry = Entry(irule, icol, 0)
-                      arr[icol].append(new_entry)
-                      rules_added[irule][icol] = True  # note that this rule already added
-
-                      print("PREDICTOR Added in Col=" + str(icol) + " Row=" + str(len(arr[icol])))
-                      grammar_rules[new_entry.rule_index].print()
-                      new_entry.print()
-            else: # if we are here, we have a completed item and we need to run ATTACH (a/k/a COMPLETE)
-              match_seeking = grammar_rules[arr[icol][irow].rule_index].lhs
-              icol2 = arr[icol][irow].start_index
-              for irow2 in range(0, len(arr[icol2])):
-                entry2 = arr[icol2][irow2]
-                if entry2.period_index < len(grammar_rules[entry2.rule_index].rhs):
-                  # then this may be seeking a completion
-                  possible_match = grammar_rules[entry2.rule_index].rhs[entry2.period_index]
-                  if possible_match == match_seeking: # if this is true, we have a "customer" to "attach"
-                    new_entry = Entry(entry2.rule_index, entry2.start_index, entry2.period_index + 1)
-                    arr[icol].append(new_entry)
-
-                    print("COMPLETER Added in Col=" + str(icol) + " Row=" + str(len(arr[icol])))
-                    grammar_rules[new_entry.rule_index].print()
-                    new_entry.print()
-
-            irow += 1
-
+    if len(sys.argv) != 3:
+        sys.exit("Usage: basic_earley grammar.gr sentences.sen")
+    parser = EarleyParser()
+    parser.read_grammar_rules(sys.argv[1])
+    parser.parse(sys.argv[2])
 
 main() # starts execution
